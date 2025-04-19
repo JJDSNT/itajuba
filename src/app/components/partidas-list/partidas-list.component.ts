71596cf109a00e4eba060cee5af9f3fe84119c85
Jaime Dias <jaimejosediasnt@gmail.com>
2025-04-19 08:10:37 -0300

    Add PartidasList componente

---
+++ src/app/components/partidas-list/partidas-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { PartidaModel } from '../../models/partida.model';
+import { CommonModule } from '@angular/common';
+import { PartidaCardComponent } from '../partida-card/partida-card.component';
+
+@Component({
+  selector: 'app-partidas-list',
+  imports: [CommonModule,PartidaCardComponent],
+  templateUrl: './partidas-list.component.html',
+  styleUrls: ['./partidas-list.component.css'],
+})
+export class PartidasListComponent {
+  @Input() partidas: PartidaModel[] = [];
+
+  get partidasAgendadas(): PartidaModel[] {
+    return this.partidas.filter((p) => p.status === 'agendada');
+  }
+
+  get partidasEncerradas(): PartidaModel[] {
+    return this.partidas.filter((p) => p.status === 'encerrada');
+  }
+}
