a14c472849380d844e555b29847ed9e1a23f8abc
Jaime Dias <jaimejosediasnt@gmail.com>
2025-04-20 10:44:54 -0300

    Add reload for new versions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,17 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'itajuba';
   currentYear = new Date().getFullYear();
+
+  private readonly updates = inject(SwUpdate);
+
+  constructor() {
+    if (this.updates.isEnabled) {
+      this.updates.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_READY') {
+          console.log('Nova versão disponível. Recarregando...');
+          document.location.reload();
+        }
+      });
+    }
+  }
 }
