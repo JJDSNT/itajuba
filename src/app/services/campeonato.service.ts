d030fe6bda3194e160f4ad7a979ea1b77e421950
Jaime Dias <jaimejosediasnt@gmail.com>
2025-04-22 17:24:40 -0300

    Fix typo

--- src/app/services/campeonato.service.ts
+++ src/app/services/campeonato.service.ts
@@ -14,8 +14,7 @@ import {
 })
 export class CampeonatoService {
   private readonly csvUrl =
-    'https://docs.google.com/spreadsheets/d/1xWApRoSOWaw_mPWIFiSI4jPp26xOpOIeDzfmxD-igA8/export?format=csv&id=1xWApRoSOWaw_mPWIFiSI4jPp26xOpOIeDzfmxD-igA8&gid=0}';
-
+    'https://docs.google.com/spreadsheets/d/1xWApRoSOWaw_mPWIFiSI4jPp26xOpOIeDzfmxD-igA8/export?format=csv&id=1xWApRoSOWaw_mPWIFiSI4jPp26xOpOIeDzfmxD-igA8&gid=0';
   private readonly clubesUrl =
     'https://docs.google.com/spreadsheets/d/1xWApRoSOWaw_mPWIFiSI4jPp26xOpOIeDzfmxD-igA8/export?format=csv&gid=29461594';
 
